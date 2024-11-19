647b7ee12e54ce9f03812d52a0de6a93b34556cd
Julio-Chacon-Amavizca <l18330415@hermosillo.tecnm.mx>
2024-11-19 12:30:54 -0700

    Se cambia ruta para imagenes del dashboard. Se cambia url de direccionamiento a vista principal de listado de usuarios.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -169,7 +169,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{url('/')}}" class="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">CRUD</span>
             </a>
@@ -179,7 +179,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{Auth::user()->name}}</a>
@@ -216,7 +216,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="{{url('/usuarios')}}" class="nav-link active">
+                                    <a href="{{url('/admin/usuarios')}}" class="nav-link active">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Listado de Usuarios</p>
                                     </a>
