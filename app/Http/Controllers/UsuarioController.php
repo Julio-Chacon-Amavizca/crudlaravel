4cf58602c64203d92d6c7f9fb5be9b7a689ebfed
Julio-Chacon-Amavizca <l18330415@hermosillo.tecnm.mx>
2024-11-19 16:35:58 -0700

    Se añaden funciones store, show, edit, update y destroy.

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class UsuarioController extends Controller
@@ -37,43 +38,75 @@ class UsuarioController extends Controller
         // return response()->json($datos);
         $request->validate([
             'name' => 'required|max:100',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|max:8|confirmed',
-            'password_verify' => 'required|same:password'
+            'email' => 'required|unique:users',
+            'password' => 'required|confirmed',
         ]);
-        
-        return $request->all();
+
+        $usuario = new User();
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = Hash::make($request->password);
+        //$usuario->password = bcrypt($request->password);
+
+        $usuario->save();
+        return redirect()->route('usuarios.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
         //
+        $usuario = User::findOrFail($id);
+        return view('admin.usuarios.show', ['usuario' => $usuario]);
+        //return $usuario;
+
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
         //
+        $usuario = User::findOrFail($id);
+        return view('admin.usuarios.edit', ['usuario' => $usuario]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
         //
+        $request->validate([
+            'name' => 'required|max:100',
+            'email' => 'required|unique:users,email,' . $id,
+            'password' => 'required|confirmed',
+        ]);
+
+        $usuario = User::find($id);
+
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = Hash::make($request->password);
+
+        $usuario->save();
+
+        return redirect()->route('usuarios.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
         //
+        User::destroy($id);
+        return redirect()->route('usuarios.index');
+        /*$usuario = User::findOrFail($id);
+        $usuario->delete();*/
+        // echo "Eliminando usuario con id: " . $id;
     }
 }
