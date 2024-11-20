ddcf1c0e98b3028c851f55198f2064aa22fd3cf4
Julio-Chacon-Amavizca <l18330415@hermosillo.tecnm.mx>
2024-11-19 17:00:41 -0700

    Se añade script de sweetalert y plantilla de mensaje.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,6 +18,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 </head>
 
@@ -169,8 +171,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{url('/')}}" class="brand-link">
-                <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
+                <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">CRUD</span>
             </a>
 
@@ -179,7 +181,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2"
+                            alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{Auth::user()->name}}</a>
@@ -256,8 +259,20 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
             <br>
-
+            @if($message = Session::get('mensaje') && ($icono = Session::get('$icono')))
+            <script>
+                Swal.fire({
+                    title: "Mensaje",
+                    text: '{{$message}}',
+                    icon: "{{$icono}}"
+                });
+            </script>
+            @endif
             <!-- Main content -->
+            <div class="container">
+                @yield('content')
+            </div>
+
             {{-- <div class="content">
                 <div class="container-fluid">
                     <div class="row">
@@ -324,9 +339,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <!-- /.row -->
                 </div><!-- /.container-fluid -->
             </div> --}}
-            <div class="container">
-                @yield('content')
-            </div>
+
             <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
